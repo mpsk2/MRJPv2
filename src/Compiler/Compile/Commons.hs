d523433b4da69080abc35519eb6249809b4fb87a
Michał Piotr Stankiewicz <ms335789@students.mimuw.edu.pl>
2017-02-01 10:32:39 +0100

    some more operations

--- src/Compiler/Compile/Commons.hs
+++ src/Compiler/Compile/Commons.hs
@@ -2,12 +2,13 @@ module Compile.Commons where
 
 import Control.Monad.State
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 import Latte.Abs
 
 import Commons
 
-data Place = Stack Int | Register String deriving (Eq)
+data Place = Stack Int | Register String deriving (Eq, Ord)
 
 type CRunner r = Runner r CState
 
@@ -16,16 +17,25 @@ instance Show Place where
     show (Register s) = s
 
 data CState = CState {
-    variables :: Map.Map String Place,
+    variables :: Map.Map String (Place, Type),
     topDefs :: Map.Map String Type,
-    lastStack :: Int
+    lastStack :: Int,
+    usedRegisters :: Set.Set Place,
+    stmtUsedRegisters :: Set.Set Place,
+    endFunctionLabel :: String
     } deriving (Show, Eq)
 
 initialCState :: CState
-initialCState = CState { variables = Map.empty, topDefs = Map.empty, lastStack = 0 }
+initialCState = CState { variables = Map.empty
+                       , topDefs = Map.empty
+                       , lastStack = 0
+                       , usedRegisters = Set.empty
+                       , stmtUsedRegisters = Set.empty
+                       , endFunctionLabel = "" }
 
 typeSize :: Type -> Int
 typeSize Int = 8
+typeSize Str = 8
 typeSize Bool = 1
 
 giveNextStack :: Type -> CRunner (Int, Int)
@@ -36,8 +46,75 @@ giveNextStack t = do
     put $ st { lastStack = ls }
     return (ls, typeSize t)
 
+getArgStack :: Int -> Int
+getArgStack n = (n - 4) * 8
+
 newFunctionStack :: CRunner ()
 newFunctionStack = do
     st <- get
     put $ st { lastStack = 0 }
+    return ()
+
+addVariable :: Type -> String -> Place -> CRunner ()
+addVariable t name place = do
+    st <- get
+    put $ st { variables = Map.insert name (place, t) (variables st) }
+    return ()
+
+callerSafe :: Place -> Bool
+callerSafe (Register "rbp") = True
+callerSafe (Register "rbx") = True
+callerSafe (Register "r12") = True
+callerSafe (Register "r13") = True
+callerSafe (Register "r14") = True
+callerSafe (Register "r15") = True
+callerSafe _ = False
+
+makeCallerSafe :: CRunner ([String], [String])
+makeCallerSafe = do
+    st <- get
+    iterateRegisters (Set.toList $ usedRegisters st) [] []
+    where
+        iterateRegisters :: [Place] -> [String] -> [String] -> CRunner ([String], [String])
+        iterateRegisters [] acc1 acc2 = return $ (acc1, reverse acc2)
+        iterateRegisters (h:t) acc1 acc2 | callerSafe h = do
+            giveNextStack Int
+            let nextBegging = (("   push  " ++ (show h) ++ " ; that register is caller safe"):acc1)
+            let nextEnding = (("   pop   " ++ (show h) ++ " ; that register is caller safe"):acc2)
+            iterateRegisters t nextBegging nextEnding
+                                         | otherwise = iterateRegisters t acc1 acc2
+
+places :: Set.Set Place
+places = Set.fromList [ Register "r15"
+                      , Register "r14"
+                      , Register "r13"
+                      , Register "r12"
+                      , Register "r11"
+                      , Register "r10"
+                      , Register "r9"
+                      , Register "r8"
+                      , Register "rcx"
+                      , Register "rdx"
+                      , Register "rsi"
+                      , Register "rdi"
+                      , Register "rdx"]
+
+givePlace :: CRunner Place
+givePlace = do
+    st <- get
+    let used = stmtUsedRegisters st
+    let freeSpace = Set.toList $ Set.difference places used
+    case freeSpace of
+        [] -> do
+            (s,_) <- giveNextStack Int
+            return $ Stack s
+        (h:_) -> do
+            put $ st { stmtUsedRegisters = Set.insert h used, usedRegisters = Set.insert h (usedRegisters st)}
+            return h
+
+restorePlace :: Place -> CRunner ()
+restorePlace (Stack _) = return ()
+restorePlace r = do
+    st <- get
+    put $ st { stmtUsedRegisters = Set.delete r $ stmtUsedRegisters st }
     return ()
--- src/Compiler/Compile/Visit.hs
+++ src/Compiler/Compile/Visit.hs
@@ -3,6 +3,7 @@ module Compile.Visit where
 import Control.Monad.Except
 import Control.Monad.State
 import qualified Data.Map as Map
+import qualified Data.Set as Set
 
 import Latte.Abs
 
@@ -32,19 +33,113 @@ rProgram (StandardProgram tds) = do
 
 rTopDef :: TopDef -> CRunner [String]
 rTopDef (FnDef ty (Ident s) args block) = do
-    newFunctionStack
+    let endLabel = s ++ "_end"
+    st_0 <- get
+    put $ st_0 { endFunctionLabel = endLabel }
+    st <- get
     let begging = functionHeader s
+
+    st_1 <- get
+    rBlock block
+    st_2 <- get
+    put $ st_1 { usedRegisters = usedRegisters st_2 }
+    (csb, csa) <- makeCallerSafe
     middle <- collectArgs args 1
+    blockContent <- rBlock block
+
     let ending = functionFooter
-    return $ begging ++ middle ++ ending
+    st2 <- get
+    liftIO $ putStrLn $ (show st2) ++ "for next function"
+    put $ st
+    return $ begging ++ csb ++ middle ++ blockContent ++ [endLabel ++ ":"] ++ csa ++ ending
     where
         collectArgs :: [Arg] -> Int -> CRunner [String]
         collectArgs [] _ = return []
         collectArgs ((NoInitArg ty (Ident s)):t) n | n < 7 = do
             (newStack, size) <- giveNextStack ty
-            -- TODO: Add variable
+            addVariable ty s (Stack newStack)
             let thatOne = ["   mov   " ++ (stackName newStack size) ++ ", " ++ (registerName (argsRegister n) size)]
             next <- (collectArgs t (n+1))
             return $ thatOne ++ next
-                                                   | otherwise = throwError $ NotImplementedError "collect args at compile"
+                                                   | otherwise = do
+            let stackNumber = getArgStack n
+            addVariable ty s (Stack stackNumber)
+            collectArgs t (n+1)
 rTopDef _ = throwError $ NotImplementedError "compile other top defs than functions"
+
+rBlock :: Block -> CRunner [String]
+rBlock (ParenBlock stmts) = do
+    st_1 <- get
+
+    _middle <- mapM rStmt stmts
+    let middle = concat _middle
+
+    st_2 <- get
+    put $ st_1 { usedRegisters = usedRegisters st_2 }
+
+    return middle
+
+rStmt :: Stmt -> CRunner [String]
+rStmt Empty = return $ []
+rStmt (BStmt block) = rBlock block
+rStmt (SDecl decl) = rDecl decl
+rStmt (Incr (Ident s)) = do
+    st <- get
+    let (place, t) = (Map.!) (variables st) s
+    case place of
+        Stack i -> return $ ["   inc   " ++ (stackName i $ typeSize t)]
+        r@(Register _) -> throwError $ NotImplementedError "compile decr register"
+rStmt (Decr (Ident s)) = do
+    st <- get
+    let (place, t) = (Map.!) (variables st) s
+    case place of
+        Stack i -> return $ ["   dec   " ++ (stackName i $ typeSize t)]
+        r@(Register _) -> throwError $ NotImplementedError "compile decr register"
+rStmt (Ret expr) = do
+    exprOutput <- rExpr expr
+    ret <- rStmt VRet
+    return $ exprOutput ++ ret
+rStmt VRet = do
+    st <- get
+    return ["   jmp " ++ (endFunctionLabel st) ]
+rStmt _ = return $ ["   nop"]
+
+rDecl :: Decl -> CRunner [String]
+rDecl (Decl t items) = do
+    _middle <- mapM (rItem t) items
+    return $ concat _middle
+rItem :: Type -> Item -> CRunner [String]
+rItem t (NoInit (Ident s)) = do
+    (st, size) <- giveNextStack t
+    addVariable t s (Stack st)
+    return $ ["   mov   " ++ (stackName st size) ++ ", 0"]
+rItem _ _ = throwError $ NotImplementedError "compile item init"
+
+rExpr :: Expr -> CRunner [String]
+rExpr (ELitInt 0) = do
+    return $ ["   xor rax, rax"]
+rExpr (ELitInt n) = return $ ["   mov rax, " ++ (show n)]
+rExpr (Neg (ELitInt n)) = rExpr (ELitInt (-n))
+rExpr (Neg expr) = do
+    e <- rExpr expr
+    return $ e ++ ["   neg rax"]
+rExpr e@(EAdd expr1 Plus expr2) = do
+    place <- givePlace
+    e1 <- rExpr expr1
+    e2 <- rExpr expr2
+    case place of
+        Stack n -> throwError $ NotImplementedError "compile add stack"
+        r@(Register _) -> do
+            restorePlace r
+            return $ e1 ++ ["   mov " ++ (show r) ++ ", rax"] ++ e2 ++ ["   add rax, " ++ (show r)]
+rExpr (EMul expr1 Times expr2) = do
+    place <- givePlace
+    e1 <- rExpr expr1
+    e2 <- rExpr expr2
+    case place of
+        Stack n -> throwError $ NotImplementedError "compile add stack"
+        r@(Register _) -> do
+            restorePlace r
+            return $ e1 ++ ["   mov " ++ (show r) ++ ", rax"] ++ e2 ++ ["   mul " ++ (show r)]
+
+rExpr _ = return $ ["   xor rax, rax ; fake end"]
